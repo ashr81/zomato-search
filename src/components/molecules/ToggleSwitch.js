e80fb1693d3a0951481ca1634f825239de094162
ashr81 <ashrithreddi81@gmail.com>
2020-03-21 09:51:32 +0530

    using css to add colors for the checkbox

--- src/components/molecules/ToggleSwitch.js
+++ src/components/molecules/ToggleSwitch.js
@@ -35,7 +35,7 @@ const ToggleSwitchLabel = styled.label`
   top: 0;
   left: 0px;
   border-radius: 15px;
-  background-color: ${({ checked, theme: { colors } }) => checked ? colors.toggleSwitchBg : colors.toggleBg};
+  background-color: ${({ theme: { colors }}) => colors.toggleBg};
   cursor: pointer;
   &::after {
     content: '';
@@ -45,9 +45,6 @@ const ToggleSwitchLabel = styled.label`
     height: 18px;
     margin: 3px;
     z-index: 2;
-    ${({ checked }) => checked && css`
-      margin-left: 30px;
-    `}
     background: #ffffff;
     box-shadow: 1px 3px 3px 1px rgba(0, 0, 0, 0.2);
     transition: 0.2s;
@@ -60,6 +57,19 @@ const CheckboxHidden = styled.input.attrs(() => ({ type: 'checkbox' }))`
   opacity: 0;
   z-index: 1;
   cursor: pointer;
+  &:checked + ${ToggleSwitchLabel} {
+    background-color: ${({ theme: { colors } }) => colors.toggleSwitchBg};
+    &:after {
+      content: '';
+      z-index: 2;
+      display: block;
+      border-radius: 50%;
+      width: 18px;
+      height: 18px;
+      margin-left: 30px;
+      transition: 0.2s;
+    }
+  }
 `;
 
 const ToggleSwitch = ({
@@ -67,8 +77,8 @@ const ToggleSwitch = ({
   pseudoElementsContent, uniqueId,
 }) => (
   <ToggleSwitchContainer checked={checked} pseudoElementsContent={pseudoElementsContent}>
-    <ToggleSwitchLabel htmlFor={uniqueId} checked={checked}/>
     <CheckboxHidden id={uniqueId} checked={checked} onChange={onChange} />
+    <ToggleSwitchLabel htmlFor={uniqueId} />
   </ToggleSwitchContainer>
 );
 
