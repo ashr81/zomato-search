d82079d1053aea6fe3904fb8ab6151b7227e5487
ashr81 <ashrithreddi81@gmail.com>
2020-03-22 15:08:57 +0530

    Wrapping list element in a search loader to avoid blink on search result load

--- src/components/organisms/restaurants/index.js
+++ src/components/organisms/restaurants/index.js
@@ -1,10 +1,11 @@
 import { lazy } from 'react';
 import withSuspenseLoader from '../../../hocs/withSuspenseLoader';
 import RestaurantsLoader from '../../../content-loaders/RestaurantsLoader';
+import SearchListLoader from '../../../content-loaders/SearchListLoader';
 
 const RestaurantsFilter = lazy(() => import(/* webpackPrefetch: true */'./Filter'));
 const RestaurantsGridView = withSuspenseLoader(lazy(() => import(/* webpackPrefetch: true */'./GridView')), RestaurantsLoader);
-const RestaurantsListElement = lazy(() => import(/* webpackPrefetch: true */'./ListElement'));
+const RestaurantsListElement = withSuspenseLoader(lazy(() => import(/* webpackPrefetch: true */'./ListElement')), SearchListLoader);
 const RestaurantsModal = withSuspenseLoader(lazy(() => import(/* webpackPrefetch: true */'./Modal')));
 
 export {
